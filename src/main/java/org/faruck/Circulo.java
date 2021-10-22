ec665ba3287e08ca461c41147a59251a88a5b047
faruckchavarria <evafaruck@gmail.com>
2021-10-21 20:17:41 -0600

    se crean valores de la clase circulo, como lados, y valor de las variables de la clase figura

--- src/main/java/org/faruck/Circulo.java
+++ src/main/java/org/faruck/Circulo.java
@@ -1,4 +1,23 @@
 package org.faruck;
 
-public class Circulo {
+import java.awt.*;
+
+public class Circulo extends Figuras {
+    public Circulo(int Lados)
+    {
+        super (Lados);
+    }
+
+    @Override
+    public double getArea() {
+
+        return 3.141592653589793D * (double)this.lado * (double)this.lado;
+    }
+
+    @Override
+    public void dibujar(Graphics G, int X, int Y)
+    {
+        G.setColor(Color.RED);
+        G.drawArc(X, Y, this.lado, this.lado, 0, 360);
+    }
 }
